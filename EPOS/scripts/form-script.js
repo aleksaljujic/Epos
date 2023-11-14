40fd00fb7d101dc14c41dd6a9e06ad5ea7dbeaf9
Aleksa Ljujić <aleksa.ljujic2@gmail.com>
2023-11-14 04:34:07 +0100

    Add files via upload

--- EPOS/scripts/form-script.js
+++ EPOS/scripts/form-script.js
@@ -3,13 +3,13 @@ function validateform(){
     let Prezime = document.getElementById('prezime').value;
     let Brojtelefona = document.getElementById('brojtelefona').value;
     let Email = document.getElementById('email').value;
-    let Datum = document.getElementById('datum').value;
+    // let Datum = document.getElementById('datum').value;
       
-    if (Ime==null || Ime==""){  
+    if (Ime==null || Ime=="" || !isUpper(Ime)){  
       alert("Niste lepo uneli ime");  
       return false;  
     }
-    if(Prezime.length<6){  
+    if(Prezime.length<6 || Prezime=="" || Prezime==null || !isUpper(Prezime)){  
       alert("Niste lepo uneli prezime");  
       return false;  
     }
@@ -18,13 +18,17 @@ function validateform(){
         return false;  
     } 
     if(Brojtelefona.length==9||Brojtelefona.length==10){
-        alert("Uspesna prijava");
+        alert(Ime + "\n" + Prezime + "\n" + Brojtelefona + "\n" + Email);
         return true;
     }else{
             alert("Niste lepo uneli broj telefona")
             return false;
     }
- }  
+ } 
+
+ function isUpper(str){
+  return /[A-Z]/.test(str);
+ }
 
 
 
--- EPOS/scripts/script.js
+++ EPOS/scripts/script.js
@@ -2,10 +2,25 @@ console.log("radi");
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const lista = document.querySelector(".nav-menu");
+const newl = document.createElement("li");
+const newl_a = document.createElement("a");
+const textNewl = document.createTextNode("Poseti nas");
+const imgs = document.getElementById("info-img");
+
+newl_a.appendChild(textNewl);
+newl_a.href = "form.html";
+newl.appendChild(newl_a);
+
+newl.classList.add("nav-item");
+
+const images = ["img/kopaonik-brdo.jpg","img/kop-noc.jpg","img/kop-staza2.jpg"];
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    newl_a.classList.add("menu-item");
+    lista.append(newl);
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
@@ -19,7 +34,12 @@ for(const element of arr){
   element.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    // lista.remove(newl);
   })
 }
+
+
+
+
 
 
